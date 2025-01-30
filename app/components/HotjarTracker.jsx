8395cefcec914415be97b3f0ef08bd630be94a8b
its-samarth <samarthkasma21@gmail.com>
2025-01-30 14:30:20 +0530

    id change

--- app/components/HotjarTracker.jsx
+++ app/components/HotjarTracker.jsx
@@ -7,7 +7,7 @@ const HotjarTracker = () => {
     if (typeof window !== 'undefined') {
       (function (h, o, t, j, a, r) {
         h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments); };
-        h._hjSettings = { hjid: 5286116, hjsv: 6 };
+        h._hjSettings = { hjid: 5286139, hjsv: 6 };
         a = o.getElementsByTagName('head')[0];
         r = o.createElement('script'); r.async = 1;
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
